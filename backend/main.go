ca490ebfda8bc5efa10f3dbe619ad59913232bb3
tetn39 <tetn39@gmail.com>
2024-09-05 11:37:49 +0900

    add: backend gin

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
+// type fruit struct {
+// 	ID   int    `json:"id"`
+// 	Name string `json:"name"`
+// 	Icon string `json:"icon"`
+// }
+
+// var fruits = []fruit{
+// 	{ID: 1, Name: "apple", Icon: "🍎"},
+// 	{ID: 2, Name: "banana", Icon: "🍌"},
+// 	{ID: 3, Name: "grape", Icon: "🍇"},
+// }
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("起動")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	r.Run(":8080")
 }
