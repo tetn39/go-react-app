fea4ce6dd93fd58c55afd830b2cb0d4669161aaf
tetn39 <tetn39@gmail.com>
2024-09-09 12:23:34 +0900

    add: tailwind css用のlint設定を追加

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,26 +3,29 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
-
+import tailwind from "eslint-plugin-tailwindcss";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     settings: {
       react: {
         version: "detect",
       },
     },
   },
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...tailwind.configs["flat/recommended"],
+
   pluginReact.configs.flat.recommended,
-  eslintConfigPrettier, 
+  eslintConfigPrettier,
   {
     rules: {
       "no-console": "off",
       "no-alert": "warn",
       "no-debugger": "warn",
-    }
-  }
+    },
+  },
 ];
